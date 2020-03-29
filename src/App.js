e5bbce111747d28589a95119ba1ba0f8aeb4e2af
Will Smith <lwilsonsmith@gmail.com>
2020-03-29 15:30:22 -0400

    Update transition effect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState }  from 'react'
-// import SmokeMachine from '@bijection/smoke'
+import { Animated } from "react-animated-css";
 import Wave from 'react-wavify'
 import arrow from './arrow.svg'
 import './App.css'
@@ -18,60 +18,63 @@ function Link(props) {
 
 function Front(props) {
   return (
-    <React.Fragment>
-      <h1>Is smoke a trigger for your asthma symptoms?</h1>
-      <p>Do you experience increased wheezing, coughing or chest tightness when exposed to smoke?</p>
-      <div>
+    <Animated className={props.className} animationOutDelay={1000} animationOut="fadeOutDown" animationIn="bounceInUp" isVisible={props.isVisible} animateOnMount={false} animationInDuration={2000}>
+      <div className="center-container">
+        <h1>Is smoke a trigger for your asthma symptoms?</h1>
+        <p>Do you experience increased wheezing, coughing or chest tightness when exposed to smoke?</p>
+      </div>
+      <div className="center-container">
         <Button text='Yes' handleClick={props.handleClick}/> <Button text='No' handleClick={props.handleClick}/>
       </div>
-    </React.Fragment>
+    </Animated>
   )
 }
 
 function Back(props) {
   return (
-    <React.Fragment>
+    <Animated className={props.className} animationIn="fadeInDown" isVisible={props.isVisible} animationInDuration={1500} animationInDelay={2000} animateOnMount={false}>
       <h1>Almost 40 million U.S. adults still smoke.</h1>
       <p>But it's not too late to quit.</p>
       <Link text='Learn More' handleClick={props.handleClick}/>
-    </React.Fragment>
+    </Animated>
   )
 }
 
 function Card(props) {
-  const firstWaveHeight = props.transitioning ? 400 : 285
-  const secondWaveHeight = props.transitioning ? 20 : 285
-
+  const opacity = props.transitioning ? "1" : "0.7"
   return (
     <div className="card-container">
     <div id={props.id} className="card">
       <h2>Symptoms Check</h2>
       <hr />
-      {props.transitioning ? <Back handleClick={props.handleClick} /> : <Front handleClick={props.handleClick} /> }
-      <Wave 
-        className={`wave ${props.transitioning && 'transitioning'}`}
-        fill="#D4EFEE"
-        opacity="0.7"
-        paused={false}
-        options={{
-          height: 285,
-          amplitude: 25,
-          speed: 0.25,
-          points: 3
-        }}
-      />
-      <Wave 
-        className={`wave ${props.transitioning && 'transitioning'}`}
-        fill="#D4EFEE"
-        opacity="0.7"
-        paused={false}
-        options={{
-          height: 285,
-          amplitude: 30,
-          speed: 0.15,
-          points: 3
-        }}
-      />
+      <Front className={`${props.transitioning ? 'hidden' : ''}`} isVisible={!props.transitioning} handleClick={props.handleClick} />
+      <Back className={`${!props.transitioning ? 'hidden' : ''}`} isVisible={props.transitioning} handleClick={props.handleClick} />
+      <div id="wave" className={`${props.transitioning ? "transition" : ""}`}>
+        <Wave 
+          className="waves"
+          fill="#D4EFEE"
+          opacity={opacity}
+          paused={false}
+          options={{
+            // height: 285,
+            amplitude: 25,
+            speed: 0.25,
+            points: 3
+          }}
+        />
+        <Wave 
+          className="waves"
+          fill="#D4EFEE"
+          opacity="0.7"
+          paused={false}
+          options={{
+            // height: 285,
+            amplitude: 30,
+            speed: 0.15,
+            points: 3
+          }}
+        />
+      </div>
     </div>
     </div>
   )
@@ -79,21 +82,15 @@ function Card(props) {
 
 function App() {
   const [isCardOneTransitioned, transitionCardOne] = useState(false)
-  const [isCardTwoTransitioned, transitionCardTwo] = useState(false)
-  const [isCardThreeTransitioned, transitionCardThree] = useState(false)
 
   const handleClick = el => {
-    el.currentTarget.offsetParent.id === "one" && transitionCardOne(!isCardOneTransitioned)
-    el.currentTarget.offsetParent.id === "two" && transitionCardTwo(!isCardTwoTransitioned)
-    el.currentTarget.offsetParent.id === "three" && transitionCardThree(!isCardThreeTransitioned)
+    transitionCardOne(!isCardOneTransitioned)
   }
 
   return (
     <main>
       <h1 className="title">Asthma Card Flip Examples</h1>
-          <Card id="one" transitioning={isCardOneTransitioned} handleClick={e => handleClick(e)}/>
-          <Card id="two" transitioning={isCardTwoTransitioned} handleClick={e => handleClick(e)}/>
-          <Card id="three" transitioning={isCardThreeTransitioned} handleClick={e => handleClick(e)}/>
+          <Card transitioning={isCardOneTransitioned} handleClick={e => handleClick(e)}/>
     </main>
   )
 }
